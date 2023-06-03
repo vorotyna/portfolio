203302a30010bee24f2f0a6e13d71585c4ff0ed5
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-06-03 11:00:16 -0400

    Style sticky to Navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -5,26 +5,28 @@ import NavbarTabs from "./NavbarTabs";
 
 const Navbar: React.FC = () => {
   return (
-    <div className=" h-[103px] flex flex-col absolute w-screen z-50">
-      {/* EXTERNAL LINKS SECTION */}
-      <section className="flex justify-end mt-6 mr-[34px]">
-        <a href="https://github.com/vorotyna" target="_blank">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/alisa-vorotyntseva/"
-          className="ml-[34px]"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-      </section>
-      {/* TAB NAVIGATION SECTION */}
-      <section className="flex justify-center">
-        <NavbarTabs name="Home" />
-        <NavbarTabs name="About" />
-        <NavbarTabs name="Projects" />
-      </section>
+    <div className="sticky top-0 z-50">
+      <div className=" h-[103px] flex flex-col w-screen absolute bg-[#F9F9FF]">
+        {/* EXTERNAL LINKS SECTION */}
+        <section className="flex justify-end mt-6 mr-[34px]">
+          <a href="https://github.com/vorotyna" target="_blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alisa-vorotyntseva/"
+            className="ml-[34px]"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </section>
+        {/* TAB NAVIGATION SECTION */}
+        <section className="flex justify-center">
+          <NavbarTabs name="Home" />
+          <NavbarTabs name="About" />
+          <NavbarTabs name="Projects" />
+        </section>
+      </div>
     </div>
   );
 };
