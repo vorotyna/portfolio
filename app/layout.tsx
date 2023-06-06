7f00abc0d2f8ae5f26906f23d72208e1525d3da8
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-06-05 21:48:44 -0400

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "./globals.css";
 import { Raleway } from "next/font/google";
 
@@ -18,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
