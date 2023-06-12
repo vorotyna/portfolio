99bba3a80c6eacafe8605032ce44572fa91d2734
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-06-12 16:03:17 -0400

    Trouble-shoot the viewport width

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body className={` ${inter.className} overflow-x-hidden`}>
+        {children}
+      </body>
     </html>
   );
 }
