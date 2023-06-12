29604233c1fde8c4cb8c7cb70240f0b7cafc4712
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-06-12 16:38:45 -0400

    Style IntroBoxes for mobile

--- app/components/About/IntroBoxes.tsx
+++ app/components/About/IntroBoxes.tsx
@@ -7,11 +7,11 @@ interface Props {
 
 const IntroBoxes: React.FC<Props> = (props: Props) => {
   return (
-    <div className="rounded-[10px] shadow-[4px_4px_24px_0px_rgba(0,0,0,0.15)] mb-16 bg-[#F9F9FF]">
-      <h3 className="text-[18px] uppercase text-center mb-[16px] mt-[30px]">
+    <div className="rounded-[10px] shadow-[4px_4px_24px_0px_rgba(0,0,0,0.15)] mobile:mb-16 mb-0 bg-[#F9F9FF]">
+      <h3 className="text-[16px] mobile:text-[18px] uppercase text-center mobile:mb-[16px] mb-[10px] mobile:mt-[30px] mt-[20px]">
         {props.header}
       </h3>
-      <p className="text-[16px] text-center mx-[45px] mb-[30px]">
+      <p className="text-[14px] mobile:text-[16px] text-center mobile:mx-[45px] mx-[15px] mobile:mb-[30px] mb-[20px]">
         {props.description}
       </p>
     </div>
